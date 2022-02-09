31f8636d465d08ff64ca656ec480de85175c8207
bys004 <bys004@msoffice365.site>
2022-02-09 20:11:09 +0300

    Geo Description

--- PhoneNumberValid/Program.cs
+++ PhoneNumberValid/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,12 +15,60 @@ namespace PhoneNumberValid
         static void Main(string[] args)
         {
             var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
-            var phoneA = phoneNumberUtil.Parse("+254720179897", "US");
-            var x = phoneNumberUtil.IsValidNumber(phoneA);
-            var countryCode = phoneNumberUtil.GetCountryCodeForRegion("KE");
-            var phoneNumberss = phoneNumberUtil.Format(phoneA, PhoneNumberFormat.INTERNATIONAL);
-            Console.WriteLine(phoneNumberss);
+            var phoneA = phoneNumberUtil.Parse("+385-975-555-405", null);
+            var x = phoneNumberUtil.IsValidNumber(phoneA); // Check if the No if valid
+            //var countryCode = phoneNumberUtil.GetCountryCodeForRegion("KE"); //From User (KE) or Code
+            var geo = PhoneNumberOfflineGeocoder.GetInstance();
+            var description = geo.GetDescriptionForNumber(phoneA, Locale.English);
+            Console.WriteLine(description);
+            //Console.WriteLine(phoneNumberUtil.ge);
+            //JsonDec();
+            Console.WriteLine(PhoneNumber("+1"));
+            GenUSNumber("Alabama");
             Thread.Sleep(-1);
+        }
+
+        static int GenUSNumber(string state)
+        {
+            //Console.Write("Enter the state: ");
+            //var code = Console.ReadLine();
+            var webclient = new WebClient();
+            var jsonFile = webclient.DownloadString("https://gist.githubusercontent.com/rkorkosz/11f16ce6493abf950e67/raw/db8ece30dc00695d46da86459a68f728f3083b3a/us_area_codes.json");
+            var c = JsonConvert.DeserializeObject<USA>(jsonFile);
+            var s = c.Alabama[0];
+            Console.WriteLine(s);
+            //foreach (var stateUsa in c)
+            //{
+            //    if (stateUsa == "Alabama")
+            //    {
+
+            //    }
+            //}
+            return 1;
+        }
+
+        private static void JsonDec()
+        {
+            var url = new WebClient();
+            var x = url.DownloadString(
+                "https://gist.githubusercontent.com/Goles/3196253/raw/9ca4e7e62ea5ad935bb3580dc0a07d9df033b451/CountryCodes.json");
+            var country = JsonConvert.DeserializeObject<List<Countries>>(x);
+            foreach (var countriese in country)
+            {
+                if (countriese.name == "Kenya")
+                {
+                    Console.WriteLine(countriese.dial_code);
+                }
+            }
+        }
+        private static readonly Random _rdm = new Random();
+        private static string PhoneNumber(string code)
+        {
+
+            var _min = (int)Math.Pow(10, 10 - 1);
+            var _max = (int)Math.Pow(10, 10) - 1;
+            return code + _rdm.Next(_min, _max).ToString();
+            
         }
     }
 }
---
+++ PhoneNumberValid/USA.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PhoneNumberValid
+{
+    class USA
+    {
+        public List<int> Mississippi { get; set; }
+
+        public List<int> NorthernMarianaIslands { get; set; }
+        public List<int> Oklahoma { get; set; }
+        public List<int> Delaware { get; set; }
+        public List<int> Minnesota { get; set; }
+        public List<int> Illinois { get; set; }
+        public List<int> Arkansas { get; set; }
+
+        [JsonProperty("New Mexico")]
+        public List<int> NewMexico { get; set; }
+        public List<int> Indiana { get; set; }
+        public List<int> Maryland { get; set; }
+        public List<int> Louisiana { get; set; }
+        public List<int> Idaho { get; set; }
+        public List<int> Wyoming { get; set; }
+        public List<int> Tennessee { get; set; }
+        public List<int> Arizona { get; set; }
+        public List<int> Iowa { get; set; }
+        public List<int> Michigan { get; set; }
+        public List<int> Kansas { get; set; }
+        public List<int> Utah { get; set; }
+
+        [JsonProperty("American Samoa")]
+        public List<int> AmericanSamoa { get; set; }
+        public List<int> Oregon { get; set; }
+        public List<int> Connecticut { get; set; }
+        public List<int> Montana { get; set; }
+        public List<int> California { get; set; }
+        public List<int> Massachusetts { get; set; }
+
+        [JsonProperty("Puerto Rico")]
+        public List<int> PuertoRico { get; set; }
+
+        [JsonProperty("South Carolina")]
+        public List<int> SouthCarolina { get; set; }
+
+        [JsonProperty("New Hampshire")]
+        public List<int> NewHampshire { get; set; }
+        public List<int> Wisconsin { get; set; }
+        public List<int> Vermont { get; set; }
+        public List<int> Georgia { get; set; }
+
+        [JsonProperty("North Dakota")]
+        public List<int> NorthDakota { get; set; }
+        public List<int> Pennsylvania { get; set; }
+
+        [JsonProperty("West Virginia")]
+        public List<int> WestVirginia { get; set; }
+        public List<int> Florida { get; set; }
+        public List<int> Hawaii { get; set; }
+        public List<int> Kentucky { get; set; }
+        public List<int> Alaska { get; set; }
+        public List<int> Nebraska { get; set; }
+        public List<int> Missouri { get; set; }
+        public List<int> Ohio { get; set; }
+        public List<int> Alabama { get; set; }
+
+        [JsonProperty("Rhode Island")]
+        public List<int> RhodeIsland { get; set; }
+
+        [JsonProperty("Washington, DC")]
+        public List<int> WashingtonDC { get; set; }
+
+        [JsonProperty("Virgin Islands")]
+        public List<int> VirginIslands { get; set; }
+
+        [JsonProperty("South Dakota")]
+        public List<int> SouthDakota { get; set; }
+        public List<int> Colorado { get; set; }
+
+        [JsonProperty("New Jersey")]
+        public List<int> NewJersey { get; set; }
+        public List<int> Virginia { get; set; }
+        public List<int> Guam { get; set; }
+        public List<int> Washington { get; set; }
+
+        [JsonProperty("North Carolina")]
+        public List<int> NorthCarolina { get; set; }
+
+        [JsonProperty("New York")]
+        public List<int> NewYork { get; set; }
+        public List<int> Texas { get; set; }
+        public List<int> Nevada { get; set; }
+        public List<int> Maine { get; set; }
+    }
+}
